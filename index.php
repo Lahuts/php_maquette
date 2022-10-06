bed5e4a53363ce0af9e60f74177776811daa756d
Lahuts <albumcroquis@gmail.com>
2022-10-06 17:01:36 +0200

    code format change

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
 <?php
 include_once './pages/var.inc.php';
-include_once './pages/head.inc.php';
+include_once './pages/head.inc.php'; 
 ?>
 <body>
-    <?php
-    include_once './pages/header.inc.php';
-    ?>
+    <?php include_once './pages/header.inc.php'; ?>
     <main>
-        <?php
-        include_once './pages/deviceInfo.inc.php';
-        ?>
+        <?php include_once './pages/deviceInfo.inc.php';?>
     </main>
     <?php include './pages/footer.inc.php'; ?>
 </body>
