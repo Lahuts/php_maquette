947a823103a821f5df56e625ca973121795ffb89
Lahuts <albumcroquis@gmail.com>
2022-10-06 16:57:43 +0200

    Maj Php full include

--- index.php
+++ index.php
@@ -7,15 +7,9 @@ include_once './pages/head.inc.php';
     include_once './pages/header.inc.php';
     ?>
     <main>
-        <section>
-            <img src="asset/sample.webp" alt="Pc Gamer">
-            <article>
-                <?php
-                include_once './pages/tab.inc.php';
-                ?>
-                <button><span class="material-symbols-outlined">shopping_cart</span>Ajouter au panier</button>
-            </article>
-        </section>
+        <?php
+        include_once './pages/deviceInfo.inc.php';
+        ?>
     </main>
     <?php include './pages/footer.inc.php'; ?>
 </body>
